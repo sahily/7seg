8dc81a1d6312f15b18b5353fa3a89dfba2cbcfff
Sahil Mahajan <sahilgotsoul@gmail.com>
2019-04-06 00:58:15 -0700

    Fixed indents

--- 7seg.c
+++ 7seg.c
@@ -21,33 +21,33 @@ sbit sl4 = P2^0;
 unsigned int numbersToDisplay[4] = {6,8,0,3};   //enter 4 numbers here
 
 unsigned int numberList[10] = {
-							      0xFC,   //0
-							      0x60,   //1
-							      0xDA,   //2 
-							      0xF2,   //3
-							      0x66,   //4
-							      0xB6,   //5
-							      0xBE,   //6
-							      0xE0,   //7
-							      0xFE,   //8
-							      0xF6    //9
-						      };
-					  		
+                                  0xFC,   //0         
+                                  0x60,   //1
+                                  0xDA,   //2 
+                                  0xF2,   //3
+                                  0x66,   //4
+                                  0xB6,   //5
+                                  0xBE,   //6
+                                  0xE0,   //7
+                                  0xFE,   //8
+                                  0xF6    //9
+                              };
+                            
 unsigned int digitSelector[4][4] = {
-								      0,1,1,1,   //select 1st digit on 7seg display
-								      1,0,1,1,   //select 2nd digit on 7seg display
-								      1,1,0,1,   //select 3rd digit on 7seg display
-								      1,1,1,0	   //select 4th digit on 7seg display
-							       };
-				  	
+                                      0,1,1,1,   //select 1st digit on 7seg display
+                                      1,0,1,1,   //select 2nd digit on 7seg display
+                                      1,1,0,1,   //select 3rd digit on 7seg display
+                                      1,1,1,0    //select 4th digit on 7seg display
+                                   };
+                    
 void main()
-{			
+{           
    init();
    while(1)
    {
       display();
    }
-	
+    
 }
   
 
@@ -78,7 +78,7 @@ void init(void)
 void delay(unsigned int k)
 {
    unsigned int i,j;
-  	
+    
    for(i=0; i<k;i++)
    {
       for(j=0;j<120;j++); 
@@ -104,11 +104,11 @@ void display(void)
       sl2 = digitSelector[x][1];
       sl3 = digitSelector[x][2];
       sl4 = digitSelector[x][3];   /* Digit is selected when its pin is reset.        */ 
-	                               /* Only one of these is selected in each iteration */
+                                   /* Only one of these is selected in each iteration */
 
       P0 = numberList[numbersToDisplay[x]];
 
       delay(4);   /* 4 milliseconds seems about the right amount of time */
-			      /* for a smooth transition to the next digit/number.	 */
+                  /* for a smooth transition to the next digit/number.   */
    }
 }
