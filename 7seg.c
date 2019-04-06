6e22691636bc161c413825926b742fd48fbd1d32
sahily <sahilgotsoul@gmail.com>
2019-04-06 00:34:19 -0700

    Update 7seg.c

--- 7seg.c
+++ 7seg.c
@@ -21,32 +21,32 @@ sbit sl4 = P2^0;
 unsigned int numbersToDisplay[4] = {6,8,0,3};   //enter 4 numbers here
 
 unsigned int numberList[10] = {
-									0xFC,       //0
-									0x60,		//1
-									0xDA,		//2 
-									0xF2,		//3
-									0x66,		//4
-									0xB6,		//5
-									0xBE,		//6
-									0xE0,		//7
-									0xFE,		//8
-									0xF6		//9
-						      };
+				0xFC,       //0
+				0x60,       //1
+				0xDA,	    //2 
+				0xF2,	    //3
+				0x66,	    //4
+				0xB6,	    //5
+				0xBE,	    //6
+				0xE0,       //7
+				0xFE,	    //8
+				0xF6	    //9
+			      };
 					  		
 unsigned int digitSelector[4][4] = {
-								0,1,1,1,		//select 1st digit on 7seg display
-								1,0,1,1,		//select 2nd digit on 7seg display
-								1,1,0,1,		//select 3rd digit on 7seg display
-								1,1,1,0			//select 4th digit on 7seg display
-							  };
+					0,1,1,1,		//select 1st digit on 7seg display
+					1,0,1,1,		//select 2nd digit on 7seg display
+					1,1,0,1,		//select 3rd digit on 7seg display
+					1,1,1,0			//select 4th digit on 7seg display
+		   		   };
 				  	
 void main()
 {			
-	init();
-	while(1)
-	{
-		display();
-	}
+   init();	
+   while(1)
+   {
+      display();
+   }
 	
 }
   
@@ -61,7 +61,7 @@ void main()
 
 void init(void)
 {
-	P0 = 0x00;
+   P0 = 0x00;
 }
 
 
@@ -77,12 +77,12 @@ void init(void)
 
 void delay(unsigned int k)
 {
-	unsigned int i,j;
+   unsigned int i,j;
   	
-	for(i=0; i<k;i++)
-  	{
-  		for(j=0;j<120;j++); 
-	}
+   for(i=0; i<k;i++)
+     {
+      for(j=0;j<120;j++); 
+     }
 
 }
 
@@ -100,14 +100,15 @@ void display(void)
 
 	for(x=0;x<4;x++)
 	{
-		sl1 = digitSelector[x][0];
-		sl2 = digitSelector[x][1];
-		sl3 = digitSelector[x][2];
-		sl4 = digitSelector[x][3];   //Digit is selected when its pin is reset. Only one of these is selected in each iteration
+	   sl1 = digitSelector[x][0];
+	   sl2 = digitSelector[x][1];
+	   sl3 = digitSelector[x][2];
+	   sl4 = digitSelector[x][3];   		/* Digit is selected when its pin is reset. Only one */ 
+							/* of these is selected in each iteration.           */
 
-		P0 = numberList[numbersToDisplay[x]];
+	   P0 = numberList[numbersToDisplay[x]];
 
-		delay(4);	 	 			/* 4 milliseconds seems about the right amount of time */
-									 /* for a smooth transition to the next digit/number.	  */
+	   delay(4);	 	 			/* 4 milliseconds seems about the right amount of time */
+							/* for a smooth transition to the next digit/number.   */
 	}
 }
